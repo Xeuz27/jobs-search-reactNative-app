71e5bc42c27cc4b0c8f7022c29100329f58b147c
Jesus <jesusg.1301.jg@gmail.com>
2023-05-09 17:49:56 -0400

    implemented the search functionality to the welcome component

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 const Home = () => {
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState('');
     return (
         <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen
@@ -25,6 +26,14 @@ const Home = () => {
                     style={{flex:1,padding: SIZES.medium}}
                 >
                     <Welcome
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    handleClick={()=> {
+                        if(searchTerm){
+                            router.push(`/search/${searchTerm}`)
+                        }
+                    }}
+
                     
                     />
                     <Popularjobs />
